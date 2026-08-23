5e6d74c840873c767bf0729997635f4438a5c072
Rahul Rewar <rrewar122@example.com>
2026-08-23 12:16:02 +0530

    fixed

--- lib/config.ts
+++ lib/config.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:4100/api/v1";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL ?? "https://social-apis.onrender.com/api/v1";
